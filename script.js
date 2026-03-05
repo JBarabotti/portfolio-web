647dd1950b7dc9f6d29d5c4412e637bd36643550
Jordan Barabotti <jordan.barabotti170104@gmail.com>
2026-03-05 11:41:58 +0100

    Mise à jour du portfolio : index.html, style.css et script.js

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ window.addEventListener("scroll", function() {
     nav.classList.remove("small");
   }
 });
+
+// Changement de langue
+const langBtn = document.getElementById("lang-btn");
+  let currentLang = "en"; // langue par défaut
+
+  langBtn.addEventListener("click", () => {
+    // bascule langue
+    currentLang = currentLang === "en" ? "fr" : "en";
+
+    // change le texte du bouton
+    langBtn.textContent = currentLang === "en" ? "Français" : "English";
+    // mettre à jour les placeholders
+    document.querySelectorAll("input[data-placeholder-en], textarea[data-placeholder-en]").forEach(el => {
+      el.placeholder = currentLang === "en" ? el.dataset.placeholderEn : el.dataset.placeholderFr;
+    });
+    // mettre à jour tous les textes visibles
+    document.querySelectorAll("[data-lang-en]").forEach(el => {
+      if(el.tagName === "INPUT" || el.tagName === "TEXTAREA"){
+        // update placeholder pour les inputs/textarea
+        el.placeholder = currentLang === "en" ? el.dataset.placeholderEn : el.dataset.placeholderFr;
+      } else {
+        // update texte normal pour h1, p, button, etc.
+        el.textContent = currentLang === "en" ? el.dataset.langEn : el.dataset.langFr;
+      }
+    });
+  });
+
+
+
